2379676d1d1797c5dcc93c4ff566df7c2c2799ef
Furkan Barım <126352270+furkanbarim@users.noreply.github.com>
2023-03-02 21:35:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+kullanıcı tarafından belirlenen 3 farklı sayının toplamının ortalamasını alınız
+1. basla
+2. 1. sayıya S1 değerini verelim
+3. 2. sayıya S2 değerini verelim
+4. 3. sayıya S3 değerini verelim
+5. S1+S2+S3 işlemini yapalım
+6. S1+S2+S3 sonucuna S4 değerini verelim
+7. S4/3 işlemini yap ve sonuca S5 değerini verelim
+8. sonuç değerini yaz
+9. bitir
+
+evde çay demleme işlemi
+a.evde çay yoksa
+b.bakkal kapalıysa
+c.sular kesik ise
+1. mutfağa git çay var mı kontrol et
+2. sular akıyor mu kontrol et
+3. evde çay yoksa komşudan çay istemeye git
+4. Çayı alıp eve geri gel
+5. suyu alıp eve geri dön
+6. çaydanlığa suyu koy
+7. ocağı yak
+8. çaydanlığı ocağa koy
+9. çaydanlık kaynadı mı? (karar mekanizması)
+10. 9 evet ise 11 e git, 9 hayır ise bekle ve 9 u kontrol et
+11. demliğe komşuda aldığın çayı koy
+12. kaynayan suyu demliğe ekle
+13. hazır oldu mu?(evet/hayır)
+14. eğer 10 evet ise 15 e git, hayır ise 13 e gitt
+15. servis yap
+16. bitir
+
+
